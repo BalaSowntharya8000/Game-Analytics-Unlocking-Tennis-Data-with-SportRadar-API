238f56e0058d2b890f957e0f399cb420ca87a94e
Bala Sowntharya <balasowntharyakb@gmail.com>
2025-04-28 22:53:36 +0530

    Update Database Tables - sportanalytics{Just for the reference}.py

---
+++ Database Tables - sportanalytics{Just for the reference}.py
@@ -0,0 +1,40 @@
+##Table: categorytable
+#category_id 
+#category_name
+
+##Table: competitiontable
+#competition_id
+#competition_name
+#parent_id
+#type
+#gender
+#category_id
+
+##Table: competitors_table
+#competitor_id
+#name
+#country
+#week
+#country_code
+#abbreviation
+
+##Table: competitor_ranking_table
+#rank_id
+#ranks
+#movement
+#points
+#competitions_played
+#competitor_id
+
+##Table: complex
+#complex_id
+#complex_name
+
+##Table: venue
+#venue_id
+#venue_name
+#city_name
+#country_name
+#country_code
+#timezone
+#complex_id
